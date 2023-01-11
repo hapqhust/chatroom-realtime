ea45af463a4fea4f444554a74a66a786d84eef1f
giangleee <giangleee>
2023-01-11 15:18:48 +0700

    client source

--- src/client.c
+++ src/client.c
@@ -11,115 +11,170 @@
 #include "proto.h"
 #include "string.h"
 
+#define STRING_NAME_LENGTH 30
+#define BUFF_SIZE 1024
+
+#define TRUE 1
+#define FALSE 0
+
 // Global variables
 volatile sig_atomic_t flag = 0;
-int sockfd = 0;
-char nickname[LENGTH_NAME] = {};
+int client_sock; // client socket
 
-void catch_ctrl_c_and_exit(int sig) {
-    flag = 1;
-}
+char nickname[STRING_NAME_LENGTH + 1];
 
-void recv_msg_handler() {
-    char receiveMessage[LENGTH_SEND] = {};
-    while (1) {
-        int receive = recv(sockfd, receiveMessage, LENGTH_SEND, 0);
-        if (receive > 0) {
-            printf("\r%s\n", receiveMessage);
-            str_overwrite_stdout();
-        } else if (receive == 0) {
-            break;
-        } else { 
-            // -1 
-        }
-    }
-}
+/**
+ * Exit on key function
+*/
+void get_ctrl_c_and_exit_button(int sig);
 
-void send_msg_handler() {
-    char message[LENGTH_MSG] = {};
-    while (1) {
-        str_overwrite_stdout();
-        while (fgets(message, LENGTH_MSG, stdin) != NULL) {
-            str_trim_lf(message, LENGTH_MSG);
-            if (strlen(message) == 0) {
-                str_overwrite_stdout();
-            } else {
-                break;
-            }
-        }
-        send(sockfd, message, LENGTH_MSG, 0);
-        if (strcmp(message, "exit") == 0) {
-            break;
-        }
-    }
-    catch_ctrl_c_and_exit(2);
-}
+void recieveMessageHandler();
 
-int main()
+void sendMessageHandler();
+
+int main(int argc, char *argv[])
 {
-    signal(SIGINT, catch_ctrl_c_and_exit);
+    signal(SIGINT, get_ctrl_c_and_exit_button);
 
-    // Naming
-    printf("Please enter your name: ");
-    if (fgets(nickname, LENGTH_NAME, stdin) != NULL) {
-        str_trim_lf(nickname, LENGTH_NAME);
-    }
-    if (strlen(nickname) < 2 || strlen(nickname) >= LENGTH_NAME-1) {
-        printf("\nName must be more than one and less than thirty characters.\n");
+    // Step 0: Get data from command
+    int SERVER_PORT, SECRET_KEY;
+    char SERVER_ADDR[BUFF_SIZE + 1];
+
+    if (argc != 4)
+    {
+        printf("Wrong command!\n");
         exit(EXIT_FAILURE);
     }
+    else
+    {
+        SERVER_PORT = atoi(argv[2]);
+        strcpy(SERVER_ADDR, argv[1]);
+        SECRET_KEY = atoi(argv[3]);
+    }
+
+    // Step 1: Construct socket
+    struct sockaddr_in server_info, client_info;
 
     // Create socket
-    sockfd = socket(AF_INET , SOCK_STREAM , 0);
-    if (sockfd == -1) {
+    client_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_sock == -1)
+    {
         printf("Fail to create a socket.");
         exit(EXIT_FAILURE);
     }
 
-    // Socket information
-    struct sockaddr_in server_info, client_info;
+    server_info.sin_family = PF_INET;
+    server_info.sin_addr.s_addr = inet_addr(SERVER_ADDR);
+    server_info.sin_port = htons(SERVER_PORT);
+
+    // Step 3: Request to connect server
+    if (connect(client_sock, (struct sockaddr *)&server_info, sizeof(struct sockaddr)) < 0)
+    {
+        printf("\nError!Can not connect to sever! Client exit imediately! ");
+        exit(EXIT_FAILURE);
+    }
+
+    // Get user name
+    printf("Please enter your name: ");
+    memset(nickname, '\0', (strlen(nickname) + 1));
+    fgets(nickname, BUFF_SIZE, stdin);
+    nickname[strcspn(nickname, "\r\n")] = 0;
+
+    if (strlen(nickname) < 2 || strlen(nickname) >= STRING_NAME_LENGTH)
+    {
+        printf("\nName length is between 1 to 30 characters.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Get socket addr
     int s_addrlen = sizeof(server_info);
     int c_addrlen = sizeof(client_info);
     memset(&server_info, 0, s_addrlen);
     memset(&client_info, 0, c_addrlen);
-    server_info.sin_family = PF_INET;
-    server_info.sin_addr.s_addr = inet_addr("127.0.0.1");
-    server_info.sin_port = htons(8888);
 
-    // Connect to Server
-    int err = connect(sockfd, (struct sockaddr *)&server_info, s_addrlen);
-    if (err == -1) {
-        printf("Connection to Server error!\n");
-        exit(EXIT_FAILURE);
-    }
-    
-    // Names
-    getsockname(sockfd, (struct sockaddr*) &client_info, (socklen_t*) &c_addrlen);
-    getpeername(sockfd, (struct sockaddr*) &server_info, (socklen_t*) &s_addrlen);
+    getsockname(client_sock, (struct sockaddr *)&client_info, (socklen_t *)&c_addrlen);
+    getpeername(client_sock, (struct sockaddr *)&server_info, (socklen_t *)&s_addrlen);
     printf("Connect to Server: %s:%d\n", inet_ntoa(server_info.sin_addr), ntohs(server_info.sin_port));
     printf("You are: %s:%d\n", inet_ntoa(client_info.sin_addr), ntohs(client_info.sin_port));
 
-    send(sockfd, nickname, LENGTH_NAME, 0);
+    // send nick name
+    send(client_sock, nickname, strlen(nickname), 0);
 
     pthread_t send_msg_thread;
-    if (pthread_create(&send_msg_thread, NULL, (void *) send_msg_handler, NULL) != 0) {
-        printf ("Create pthread error!\n");
+    if (pthread_create(&send_msg_thread, NULL, (void *)sendMessageHandler, NULL) != 0)
+    {
+        printf("Create pthread error!\n");
         exit(EXIT_FAILURE);
     }
 
     pthread_t recv_msg_thread;
-    if (pthread_create(&recv_msg_thread, NULL, (void *) recv_msg_handler, NULL) != 0) {
-        printf ("Create pthread error!\n");
+    if (pthread_create(&recv_msg_thread, NULL, (void *)recieveMessageHandler, NULL) != 0)
+    {
+        printf("Create pthread error!\n");
         exit(EXIT_FAILURE);
     }
 
-    while (1) {
-        if(flag) {
+    while (TRUE)
+    {
+        if (flag)
+        {
             printf("\nBye\n");
             break;
         }
     }
 
-    close(sockfd);
+    close(client_sock);
     return 0;
+}
+
+void get_ctrl_c_and_exit_button(int sig)
+{
+    flag = 1;
+}
+
+void recieveMessageHandler()
+{
+    char receiveMessage[BUFF_SIZE];
+    while (TRUE)
+    {
+        int receive = recv(client_sock, receiveMessage, BUFF_SIZE, 0);
+        if (receive > 0)
+        {
+            printf("\r%s\n", receiveMessage);
+            str_overwrite_stdout();
+        }
+        else if (receive == 0)
+        {
+            printf("Connection closed.\n");
+        }
+        else
+            perror("\nError: ");
+    }
+}
+
+void sendMessageHandler()
+{
+    char message[BUFF_SIZE];
+    while (TRUE)
+    {
+        str_overwrite_stdout();
+        while (fgets(message, BUFF_SIZE, stdin) != NULL)
+        {
+            str_trim_lf(message, BUFF_SIZE);
+            if (strlen(message) == 0)
+            {
+                str_overwrite_stdout();
+            }
+            else
+            {
+                break;
+            }
+        }
+        send(client_sock, message, BUFF_SIZE, 0);
+        if (strcmp(message, "exit") == 0)
+        {
+            break;
+        }
+    }
+    get_ctrl_c_and_exit_button(2);
 }
